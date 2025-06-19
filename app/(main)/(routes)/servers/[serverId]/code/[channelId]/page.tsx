4adef3877746769086a5866c5f0262d972453124
Yaroslav Yazykov <fenathesnake@gmail.com>
2025-06-19 14:04:29 +0700

    voicechat

--- app/(main)/(routes)/servers/[serverId]/code/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/code/[channelId]/page.tsx
@@ -2,15 +2,7 @@ import CodeSessionEditor from "@/components/editor/monaco-editor";
 
 const CodePage = ({params} : {params: {serverId: string, channelId: string}}) => {
     return (  
-        <div>
-            Server ID Page.
-            {/* <main className="flex justify-center items-start min-h-screen bg-gray-100">
-            <div className="w-full max-w-4xl mt-10">
-                <h1 className="text-2xl font-bold mb-4">Совместный редактор кода</h1> */}
-                <CodeSessionEditor serverId={params.serverId} channelId={params.channelId} />
-            {/* </div>
-            </main> */}
-        </div>
+        <CodeSessionEditor serverId={params.serverId} channelId={params.channelId} />
     );
 }
  
